9218c76324ee05c0b3715f8d3720024b10afd1f8
merlox <merunasgrincalaitis@gmail.com>
2019-02-04 13:02:45 +0100

    Improved documentation

--- src/js/index.js
+++ src/js/index.js
@@ -25,7 +25,16 @@ class App extends React.Component {
             <p>NOTE: the .toString('hex') at the end</p>
             <p>Example:</p>
             <pre>
-               abi.rawEncode(['uint256','string'], ['2345675643', 'Hello!%']).toString('hex');
+               abi.rawEncode(['uint256','string'], ['2345675643', 'Hello!']).toString('hex');
+
+               // Results in
+               000000000000000000000000000000000000000000000000000000008bd02b7b0000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000648656c6c6f210000000000000000000000000000000000000000000000000000
+
+               // Remember to add your function signature from remix, for instance if your function is: function setMyNumber(uint256 _myNumber) then the signature will be
+               0x6ffd773c
+
+               // Combine both and you get the valid msg.data that you can use with delegatecall(msg.data)
+               0x6ffd773c000000000000000000000000000000000000000000000000000000008bd02b7b0000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000648656c6c6f210000000000000000000000000000000000000000000000000000
             </pre>
          </div>
       )
